99a698b971a6d5ca3fd24a00a7e1554f84ead9fa
nameer94 <32635299+nameer94@users.noreply.github.com>
2021-09-05 00:27:24 +0300

    Fix merge conflict

--- types.tsx
+++ types.tsx
@@ -16,6 +16,7 @@ declare global {
 export type RootStackParamList = {
   Root: NavigatorScreenParams<RootTabParamList> | undefined;
   Home: undefined;
+  Assignments: undefined;
   Start: undefined;
   Login: undefined;
   Modal: undefined;
