a9cf46c04b312f6d6270065109e613d096e4d961
Mohammed Salman <msal4@outlook.com>
2021-09-08 23:12:41 +0300

    Revert to the old chat screen

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -16,7 +16,7 @@ export function ChatScreen() {
   return (
     <View style={{}}>
       {res.data?.groups && res.data.groups.edges?.map((g) => <ChatGroup key={g?.node?.id} group={g?.node!} />)}
-      <Error isError={!res.error} onPress={refetch} />
+      <Error isError={!!res.error} onPress={refetch} />
       <Loading isLoading={res.fetching} />
     </View>
   );
