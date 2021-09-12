a4005566813c6f0541141ca70cc6323470e88738
Mohammed Salman <msal4@outlook.com>
2021-09-13 02:26:06 +0300

    Fix profile screen

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -5,7 +5,7 @@ import { clearTokens } from "../util/auth";
 import { RootTabScreenProps } from "../types";
 import { saveCurrentUser } from "../hooks/useMe";
 import { useAuth } from "../context/auth";
-import { replace, navigationRef } from "../navigation/navigationRef";
+import { replace } from "../navigation/navigationRef";
 import { useProfileQuery } from "../generated/graphql";
 import Loading from "../components/Loading";
 import { Error } from "../components/Error";
@@ -16,12 +16,16 @@ import SelectModal from "../components/Select";
 import { Ionicons } from "@expo/vector-icons";
 import { cdnURL } from "../constants/Config";
 import { Locale } from "../hooks/useLocale";
+import dayjs from "dayjs";
 
 export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => {
   const { t, locale, setLocale } = useTrans();
   const { setIsAuthenticated } = useAuth();
   const [res, refetch] = useProfileQuery();
   const [showDialog, setShowDialog] = useState(false);
+  const [showClassesDialog, setShowClassesDialog] = useState(false);
+
+  console.log(res);
 
   const numberWithCommas = (x: number) => {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -58,6 +62,7 @@ export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => 
         }}
       >
         <View style={{ backgroundColor: "#fff", flex: 1, borderRadius: 20, overflow: "hidden", padding: 10 }}>
+          <KText style={{ textAlign: "left", color: "#000", paddingHorizontal: 20 }}>{t("payments")}</KText>
           {res.data?.me.stage?.payments?.edges && res.data?.me.stage?.payments?.edges?.length > 0 ? (
             <FlatList
               data={res.data?.me.stage?.payments?.edges}
@@ -67,9 +72,12 @@ export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => 
               renderItem={({ item }) => (
                 <View style={{ flexDirection: "row", padding: 20, borderBottomWidth: 1, borderBottomColor: "#ddd" }}>
                   <View style={{ flex: 1 }}>
-                    <Text style={{ fontFamily: "Dubai-Bold", color: "#000", textAlign: "left" }}>{item?.node?.year}</Text>
+                    <Text style={{ fontFamily: "Dubai-Bold", color: "#000", textAlign: "left" }}>
+                      {t("schoolـyear")} {item?.node?.year}
+                    </Text>
                     <Text style={{ fontFamily: "Dubai-Bold", color: "#919191", textAlign: "left" }} numberOfLines={1}>
-                      {numberWithCommas(item?.node?.paidAmount)}
+                      {numberWithCommas(item?.node?.paidAmount)} {t("iqd")} -{" "}
+                      {dayjs(item?.node?.createdAt).locale(locale).format("D - MMMM - YYYY")}
                     </Text>
                   </View>
                 </View>
@@ -79,6 +87,20 @@ export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => 
         </View>
       </Dialog>
 
+      <Dialog
+        migrate
+        useSafeArea
+        bottom={true}
+        height={500}
+        panDirection={"UP"}
+        visible={showClassesDialog}
+        onDismiss={() => {
+          setShowClassesDialog(false);
+        }}
+      >
+        <Mymarks res={res} />
+      </Dialog>
+
       {!res.fetching ? (
         <View style={{ flex: 1, backgroundColor: "#fff" }}>
           <View
@@ -86,8 +108,6 @@ export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => 
               backgroundColor: "#f4f4f4",
               flexDirection: "row",
               alignItems: "center",
-              borderBottomWidth: 1,
-              borderBottomColor: "#e5e5e5",
               padding: 20,
             }}
           >
@@ -168,7 +188,6 @@ export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => 
                   },
                 ]);
               }}
-              style={{ borderBottomWidth: 1, borderBottomColor: "#e5e5e5" }}
             >
               <View style={{ padding: 20, flexDirection: "row", alignItems: "center" }}>
                 <Ionicons name="log-out-outline" size={22} color="#777" />
@@ -182,3 +201,28 @@ export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => 
     </>
   );
 };
+
+function Mymarks({ res }) {
+  return (
+    <View style={{ backgroundColor: "#fff", flex: 1, borderRadius: 20, overflow: "hidden", padding: 10 }}>
+      {res.data?.me.stage?.classes?.edges && res.data?.me.stage?.classes?.edges?.length > 0 ? (
+        <FlatList
+          data={res.data?.me.stage?.classes?.edges}
+          keyExtractor={(item, index) => index + "a"}
+          contentContainerStyle={{ paddingBottom: 20 }}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <View style={{ flexDirection: "row", padding: 20, borderBottomWidth: 1, borderBottomColor: "#ddd" }}>
+              <View style={{ flex: 1 }}>
+                <Text style={{ fontFamily: "Dubai-Bold", color: "#000", textAlign: "left" }}>{item?.node?.name}</Text>
+                <Text style={{ fontFamily: "Dubai-Bold", color: "#919191", textAlign: "left" }} numberOfLines={1}>
+                  {item?.node?.teacher.name}
+                </Text>
+              </View>
+            </View>
+          )}
+        />
+      ) : null}
+    </View>
+  );
+}
