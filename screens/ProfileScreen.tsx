609925a775d2cc72c64fbc569f8db5d3bb4764a5
Mohammed Salman <msal4@outlook.com>
2021-09-13 20:17:03 +0300

    profile: fix styling issues in payment

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -11,7 +11,7 @@ import Loading from "../components/Loading";
 import { Error } from "../components/Error";
 import { View, Alert, ScrollView, Text, FlatList } from "react-native";
 import { KText } from "../components/KText";
-import { Image, Dialog } from "react-native-ui-lib";
+import { Image, Dialog, PanningDirections, PanningProvider } from "react-native-ui-lib";
 import SelectModal from "../components/Select";
 import { Ionicons } from "@expo/vector-icons";
 import { cdnURL } from "../constants/Config";
@@ -49,32 +49,32 @@ export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => 
   return (
     <>
       <Dialog
-        migrate
         useSafeArea
         bottom={true}
         height={300}
-        panDirection={"UP"}
+        panDirection={PanningProvider.Directions.DOWN}
         visible={showDialog}
         onDismiss={() => {
           setShowDialog(false);
         }}
       >
-        <View style={{ backgroundColor: "#fff", flex: 1, borderRadius: 20, overflow: "hidden", padding: 10 }}>
-          <KText style={{ textAlign: "left", color: "#000", paddingHorizontal: 20 }}>{t("payments")}</KText>
+        <View style={{ backgroundColor: "#fff", flex: 1, borderRadius: 20, overflow: "hidden" }}>
+          <View style={{ backgroundColor: "#f4f4f4", flexDirection: "row", paddingHorizontal: 20, paddingVertical: 10 }}>
+            <KText style={{ fontSize: 23, color: "#393939" }}>{t("payments")}</KText>
+          </View>
           {res.data?.me.stage?.payments?.edges && res.data?.me.stage?.payments?.edges?.length > 0 ? (
             <FlatList
               data={res.data?.me.stage?.payments?.edges}
-              keyExtractor={(item, index) => index + "a"}
+              keyExtractor={(item) => item?.node?.id ?? ""}
               contentContainerStyle={{ paddingBottom: 20 }}
               showsVerticalScrollIndicator={false}
+              ItemSeparatorComponent={() => <View style={{ borderBottomWidth: 1, borderBottomColor: "#ddd" }} />}
               renderItem={({ item }) => (
-                <View style={{ flexDirection: "row", padding: 20, borderBottomWidth: 1, borderBottomColor: "#ddd" }}>
+                <View style={{ flexDirection: "row", padding: 20 }}>
                   <View style={{ flex: 1 }}>
-                    <Text style={{ fontFamily: "Dubai-Medium", color: "#000", textAlign: "left" }}>
-                      {t("schoolـyear")} {item?.node?.year}
-                    </Text>
+                    <Text style={{ fontFamily: "Dubai-Medium", color: "#383838", textAlign: "left" }}>{item?.node?.year}</Text>
                     <Text style={{ fontFamily: "Dubai-Regular", color: "#919191", textAlign: "left" }} numberOfLines={1}>
-                      {numberWithCommas(item?.node?.paidAmount)} {t("iqd")} -{" "}
+                      {numberWithCommas(item?.node?.paidAmount ?? 0)} {t("iqd")} -{" "}
                       {dayjs(item?.node?.createdAt).locale(locale).format("D - MMMM - YYYY")}
                     </Text>
                   </View>
@@ -103,7 +103,7 @@ export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => 
               borderRadius={100}
             />
             <View>
-              <KText style={{ color: "#000", textAlign: "left" }}>{res.data?.me.name}</KText>
+              <KText style={{ color: "#383838", textAlign: "left" }}>{res.data?.me.name}</KText>
               <KText style={{ color: "#393939", textAlign: "left" }}>
                 {res.data?.me.school?.name} {stageName ? "-" : ""} {stageName}
               </KText>
@@ -118,7 +118,7 @@ export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => 
             >
               <View style={{ padding: 20, flexDirection: "row", alignItems: "center" }}>
                 <Ionicons name="ios-checkmark-done-circle-outline" size={22} color="#777" />
-                <KText style={{ textAlign: "left", color: "#000", paddingHorizontal: 20 }}>{t("my_marks")}</KText>
+                <KText style={{ textAlign: "left", color: "#383838", paddingHorizontal: 20 }}>{t("my_marks")}</KText>
               </View>
             </Touchable>
             <Touchable
@@ -129,7 +129,7 @@ export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => 
             >
               <View style={{ padding: 20, flexDirection: "row", alignItems: "center" }}>
                 <Ionicons name="card-outline" size={22} color="#777" />
-                <KText style={{ textAlign: "left", color: "#000", paddingHorizontal: 20 }}>{t("payments")}</KText>
+                <KText style={{ textAlign: "left", color: "#383838", paddingHorizontal: 20 }}>{t("payments")}</KText>
               </View>
             </Touchable>
             <View style={{ borderBottomWidth: 1, borderBottomColor: "#e5e5e5" }}>
@@ -146,7 +146,7 @@ export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => 
                   <View style={{ padding: 20, flexDirection: "row", alignItems: "center" }}>
                     <Ionicons name="ios-globe-outline" size={22} color="#777" />
                     <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
-                      <KText style={{ textAlign: "left", color: "#000", paddingHorizontal: 20 }}>{t("language")}</KText>
+                      <KText style={{ textAlign: "left", color: "#383838", paddingHorizontal: 20 }}>{t("language")}</KText>
                       <KText style={{ textAlign: "left", color: "#aaa", paddingHorizontal: 20 }}>
                         {locale === "ar" ? "العربية" : "English"}
                       </KText>
@@ -175,7 +175,7 @@ export const ProfileScreen = ({ navigation }: RootTabScreenProps<"Profile">) => 
             >
               <View style={{ padding: 20, flexDirection: "row", alignItems: "center" }}>
                 <Ionicons name="log-out-outline" size={22} color="#777" />
-                <KText style={{ textAlign: "left", color: "#000", paddingHorizontal: 20 }}>{t("logout")}</KText>
+                <KText style={{ textAlign: "left", color: "#383838", paddingHorizontal: 20 }}>{t("logout")}</KText>
               </View>
             </Touchable>
           </ScrollView>
