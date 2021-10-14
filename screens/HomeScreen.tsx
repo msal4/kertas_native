f8c533d9aec1234be124cf8422b0202496ff364b
Mohammed Salman <msal4@outlook.com>
2021-10-14 10:03:03 +0300

    Add image background to home page

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
 import { useState } from "react";
-import { View, Dimensions, FlatList, Platform, TouchableOpacity } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  View,
+  Dimensions,
+  FlatList,
+  Platform,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 import { Ionicons as Icon } from "@expo/vector-icons";
 import ScrollBottomSheet from "react-native-scroll-bottom-sheet";
 import { Touchable } from "../components/Touchable";
@@ -32,173 +39,233 @@ export default function Home({ navigation }: RootStackScreenProps<"Home">) {
   const [selectedWeekday, setWeekDay] = useState(dayjs().day());
   const { t, locale } = useTrans();
   const { me } = useMe();
+  const { top, right, left } = useSafeAreaInsets();
 
   const currDate = dayjs().add(selectedWeekday - dayjs().day(), "day");
 
   return (
-    <SafeAreaView style={{ backgroundColor: "#f4f4f4", flex: 1 }}>
-      <View
-        style={{
-          backgroundColor: "#f4f4f4" + "dd",
-          paddingTop: 20,
-          paddingBottom: 10,
-          paddingHorizontal: 20,
-          flexDirection: "row",
-          alignItems: "center",
-          position: "absolute",
-          width: "100%",
-          zIndex: 999,
-          marginTop: getStatusBarHeight(),
-        }}
-      >
-        <View style={{ flex: 1 }}>
-          <KText style={{ fontFamily: "Dubai-Medium", color: "#393939", fontSize: 35, textAlign: "left" }}>{currDate.format("dddd")}</KText>
-          <KText style={{ fontFamily: "Dubai-Regular", color: "#393939", textAlign: "left" }}>{currDate.format("D - MMMM - YYYY")}</KText>
-        </View>
-        <SelectModal
-          data={dayjs_locals[locale].weekdays!.map((name, value) => ({ name, value }))}
-          onSelect={({ value }) => {
-            setWeekDay(value as number);
+    <ImageBackground
+      source={require("../assets/images/chat_background.jpg")}
+      style={{ flex: 1 }}
+      imageStyle={{ opacity: 0.1 }}
+    >
+      <SafeAreaView style={{ flex: 1 }}>
+        <View
+          style={{
+            backgroundColor: "#f4f4f4aa",
+            paddingBottom: 10,
+            paddingRight: 20 + right,
+            paddingLeft: 20 + left,
+            flexDirection: "row",
+            alignItems: "center",
+            position: "absolute",
+            width: "100%",
+            zIndex: 999,
+            paddingTop: top + 20,
           }}
-          selected={selectedWeekday}
-          renderBtn={() => (
-            <View
+        >
+          <View style={{ flex: 1 }}>
+            <KText
               style={{
-                backgroundColor: "#fff",
-                borderRadius: 100,
-                width: 50,
-                height: 50,
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center",
+                fontFamily: "Dubai-Medium",
+                color: "#393939",
+                fontSize: 35,
+                textAlign: "left",
               }}
             >
-              <Icon name="calendar-outline" size={28} color="#a18cd1" />
-            </View>
-          )}
-          initialNumToRender={7}
-          height={600}
-        />
-      </View>
-
-      <View style={{ flex: 1, backgroundColor: "#f4f4f4" }}>
-        <Schedule weekday={selectedWeekday} />
-      </View>
-
-      <ScrollBottomSheet<string>
-        componentType="ScrollView"
-        snapPoints={[
-          windowHeight - 427 + (Platform.OS == "ios" ? getStatusBarHeight() : -getStatusBarHeight()),
-          windowHeight - 270 + (Platform.OS == "ios" ? getStatusBarHeight() : -getStatusBarHeight()),
-        ]}
-        initialSnapIndex={0}
-        renderHandle={() => null}
-        showsVerticalScrollIndicator={false}
-      >
-        <View style={{ backgroundColor: "#fff", flex: 1, borderTopLeftRadius: 20, borderTopRightRadius: 20, overflow: "hidden" }}>
-          <View style={{ flexDirection: "row" }}>
-            <View style={{ flex: 1 }}>
-              <Touchable
-                onPress={() => {
-                  navigation.navigate("Assignments", { isExam: false });
-                }}
-              >
-                <View style={{ backgroundColor: "#fff", justifyContent: "center", alignItems: "center", height: 150 }}>
-                  <View
-                    style={{
-                      backgroundColor: "#f4f4f4",
-                      borderRadius: 100,
-                      width: 50,
-                      height: 50,
-                      flexDirection: "row",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Icon name="reader-outline" size={28} color="#a18cd1" />
-                  </View>
-                  <KText style={{ color: "#393939", fontSize: 18 }}>{t("assignments")}</KText>
-                </View>
-              </Touchable>
-            </View>
-            <View style={{ flex: 1 }}>
-              <Touchable
-                onPress={() => {
-                  navigation.navigate("Assignments", { isExam: true });
-                }}
-              >
-                <View style={{ backgroundColor: "#fafafa", justifyContent: "center", alignItems: "center", height: 150 }}>
-                  <View
-                    style={{
-                      backgroundColor: "#f4f4f4",
-                      borderRadius: 100,
-                      width: 50,
-                      height: 50,
-                      flexDirection: "row",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Icon name="create-outline" size={28} color="#a18cd1" />
-                  </View>
-                  <KText style={{ color: "#393939", fontSize: 18 }}>{t("exams")}</KText>
-                </View>
-              </Touchable>
-            </View>
+              {currDate.format("dddd")}
+            </KText>
+            <KText style={{ fontFamily: "Dubai-Regular", color: "#393939", textAlign: "left" }}>
+              {currDate.format("D - MMMM - YYYY")}
+            </KText>
           </View>
-          <View style={{ flexDirection: "row" }}>
-            <View style={{ flex: 1 }}>
-              <Touchable
-                onPress={() => {
-                  navigation.navigate("CourseGrades");
+          <SelectModal
+            data={dayjs_locals[locale].weekdays!.map((name, value) => ({ name, value }))}
+            onSelect={({ value }) => {
+              setWeekDay(value as number);
+            }}
+            selected={selectedWeekday}
+            renderBtn={() => (
+              <View
+                style={{
+                  backgroundColor: "#fff",
+                  borderRadius: 100,
+                  width: 50,
+                  height: 50,
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
                 }}
               >
-                <View style={{ backgroundColor: "#fafafa", justifyContent: "center", alignItems: "center", height: 150 }}>
-                  <View
-                    style={{
-                      backgroundColor: "#f4f4f4",
-                      borderRadius: 100,
-                      width: 50,
-                      height: 50,
-                      flexDirection: "row",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Icon name="ios-checkmark-done-circle-outline" size={28} color="#a18cd1" />
-                  </View>
-                  <KText style={{ color: "#393939", fontSize: 18 }}>{t(me?.role === "TEACHER" ? "marks" : "my_marks")}</KText>
-                </View>
-              </Touchable>
-            </View>
-            <View style={{ flex: 1 }}>
-              <Touchable
-                onPress={() => {
-                  navigation.navigate("Attendance");
-                }}
-              >
-                <View style={{ backgroundColor: "#fff", justifyContent: "center", alignItems: "center", height: 150 }}>
-                  <View
-                    style={{
-                      backgroundColor: "#f4f4f4",
-                      borderRadius: 100,
-                      width: 50,
-                      height: 50,
-                      flexDirection: "row",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <Icon name="people-outline" size={28} color="#a18cd1" />
-                  </View>
-                  <KText style={{ color: "#393939", fontSize: 18 }}>{t("attendance")}</KText>
-                </View>
-              </Touchable>
-            </View>
-          </View>
+                <Icon name="calendar-outline" size={28} color="#a18cd1" />
+              </View>
+            )}
+            initialNumToRender={7}
+            height={600}
+          />
         </View>
-      </ScrollBottomSheet>
-    </SafeAreaView>
+
+        <View style={{ flex: 1 }}>
+          <Schedule weekday={selectedWeekday} />
+        </View>
+
+        <ScrollBottomSheet<string>
+          componentType="ScrollView"
+          snapPoints={[
+            windowHeight -
+              427 +
+              (Platform.OS == "ios" ? getStatusBarHeight() : -getStatusBarHeight()),
+            windowHeight -
+              270 +
+              (Platform.OS == "ios" ? getStatusBarHeight() : -getStatusBarHeight()),
+          ]}
+          initialSnapIndex={0}
+          renderHandle={() => null}
+          showsVerticalScrollIndicator={false}
+        >
+          <View
+            style={{
+              backgroundColor: "#fff",
+              flex: 1,
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+              overflow: "hidden",
+            }}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <View style={{ flex: 1 }}>
+                <Touchable
+                  onPress={() => {
+                    navigation.navigate("Assignments", { isExam: false });
+                  }}
+                >
+                  <View
+                    style={{
+                      backgroundColor: "#fff",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: 150,
+                    }}
+                  >
+                    <View
+                      style={{
+                        backgroundColor: "#f4f4f4",
+                        borderRadius: 100,
+                        width: 50,
+                        height: 50,
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Icon name="reader-outline" size={28} color="#a18cd1" />
+                    </View>
+                    <KText style={{ color: "#393939", fontSize: 18 }}>{t("assignments")}</KText>
+                  </View>
+                </Touchable>
+              </View>
+              <View style={{ flex: 1 }}>
+                <Touchable
+                  onPress={() => {
+                    navigation.navigate("Assignments", { isExam: true });
+                  }}
+                >
+                  <View
+                    style={{
+                      backgroundColor: "#fafafa",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: 150,
+                    }}
+                  >
+                    <View
+                      style={{
+                        backgroundColor: "#f4f4f4",
+                        borderRadius: 100,
+                        width: 50,
+                        height: 50,
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Icon name="create-outline" size={28} color="#a18cd1" />
+                    </View>
+                    <KText style={{ color: "#393939", fontSize: 18 }}>{t("exams")}</KText>
+                  </View>
+                </Touchable>
+              </View>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              <View style={{ flex: 1 }}>
+                <Touchable
+                  onPress={() => {
+                    navigation.navigate("CourseGrades");
+                  }}
+                >
+                  <View
+                    style={{
+                      backgroundColor: "#fafafa",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: 150,
+                    }}
+                  >
+                    <View
+                      style={{
+                        backgroundColor: "#f4f4f4",
+                        borderRadius: 100,
+                        width: 50,
+                        height: 50,
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Icon name="ios-checkmark-done-circle-outline" size={28} color="#a18cd1" />
+                    </View>
+                    <KText style={{ color: "#393939", fontSize: 18 }}>
+                      {t(me?.role === "TEACHER" ? "marks" : "my_marks")}
+                    </KText>
+                  </View>
+                </Touchable>
+              </View>
+              <View style={{ flex: 1 }}>
+                <Touchable
+                  onPress={() => {
+                    navigation.navigate("Attendance");
+                  }}
+                >
+                  <View
+                    style={{
+                      backgroundColor: "#fff",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      height: 150,
+                    }}
+                  >
+                    <View
+                      style={{
+                        backgroundColor: "#f4f4f4",
+                        borderRadius: 100,
+                        width: 50,
+                        height: 50,
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Icon name="people-outline" size={28} color="#a18cd1" />
+                    </View>
+                    <KText style={{ color: "#393939", fontSize: 18 }}>{t("attendance")}</KText>
+                  </View>
+                </Touchable>
+              </View>
+            </View>
+          </View>
+        </ScrollBottomSheet>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
 
@@ -247,8 +314,22 @@ function Schedule({ weekday }: { weekday: number }) {
                 navigation.navigate("Conversation", { groupID: item.class.group.id });
               }}
             >
-              <View style={{ flexDirection: "row", padding: 15, backgroundColor: "#fff", alignItems: "center" }}>
-                <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", paddingRight: 10 }}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  padding: 15,
+                  backgroundColor: "#fff",
+                  alignItems: "center",
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    paddingRight: 10,
+                  }}
+                >
                   <View
                     style={{
                       backgroundColor: getClassTime(item).isNow == true ? "#5fc414" : "transparent",
@@ -258,9 +339,22 @@ function Schedule({ weekday }: { weekday: number }) {
                     }}
                   ></View>
                 </View>
-                <KText style={{ fontFamily: "Dubai-Medium", color: "#393939" }}>{item.class.name}</KText>
-                <View style={{ width: 1, backgroundColor: "#777", marginHorizontal: 10, height: 15 }}></View>
-                <KText style={{ fontFamily: "Dubai-Regular", color: "#777", flex: 1, textAlign: "left", fontSize: 12 }} numberOfLines={1}>
+                <KText style={{ fontFamily: "Dubai-Medium", color: "#393939" }}>
+                  {item.class.name}
+                </KText>
+                <View
+                  style={{ width: 1, backgroundColor: "#777", marginHorizontal: 10, height: 15 }}
+                ></View>
+                <KText
+                  style={{
+                    fontFamily: "Dubai-Regular",
+                    color: "#777",
+                    flex: 1,
+                    textAlign: "left",
+                    fontSize: 12,
+                  }}
+                  numberOfLines={1}
+                >
                   {item.class.teacher.name}
                 </KText>
                 <KText style={{ fontFamily: "Dubai-Regular", color: "#393939", fontSize: 12 }}>
